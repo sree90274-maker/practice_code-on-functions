bb7869869cac9e5ce44af2e1656165c35557aa08
sree90274-maker <sree90274@gmail.com>
2026-07-02 03:58:17 +0530

    function_using_arguments

---
+++ with_arguments.py
@@ -0,0 +1,21 @@
+#Add teo numbers
+def add(a,b):
+	print("sum:",a+b)
+add(10,20)
+#Divide two numbers
+def divide(a,b):
+	print("Quotient:",a/b)
+divide(20,5)
+#Exponential of a number
+def exponential(n):
+	print(n**n)
+exponential(5)
+#calucate the total marks of five subjects
+def total_marks(s1,s2,S3,s4,s5):
+	print(s1+s2+S3+s4+s5)
+print(50,70,40,80,90)
+#find avg of numbers
+def average(a,b,c):
+	avg=a+b+c/3
+	print(avg)
+average(20,30,40)
